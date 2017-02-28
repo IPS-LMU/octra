ffba5fbd140160eb8ac50197ea8ab1099b7457de
Julian Pömp <j.poemp@campus.lmu.de>
2017-02-28 18:48:35 +0100

    dropzone now supports selecting one file on click

--- src/app/component/drop-zone/drop-zone.component.ts
+++ src/app/component/drop-zone/drop-zone.component.ts
@@ -1,53 +1,76 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { SessionFile } from "../../shared/SessionFile";
 
+declare var window: any;
+
 @Component({
-  selector: 'app-drop-zone',
-  templateUrl: './drop-zone.component.html',
-  styleUrls: ['./drop-zone.component.css']
+	selector   : 'app-drop-zone',
+	templateUrl: './drop-zone.component.html',
+	styleUrls  : [ './drop-zone.component.css' ]
 })
-
 export class DropZoneComponent implements OnInit {
-  get file(): File {
-    return this._file;
-  }
+	get file(): File {
+		return this._file;
+	}
 
-  get sessionfile(): SessionFile {
-    return this._sessionfile;
-  }
+	get sessionfile(): SessionFile {
+		return this._sessionfile;
+	}
 
-  @Input()
-  innerhtml:string = "";
+	@Input()
+	innerhtml: string = "";
 
-  private _file:File;
-  private _sessionfile:SessionFile;
+	private _file: File;
+	private _sessionfile: SessionFile;
+	private fileAPIsupported = false;
 
-  @Output()
-  public afterdrop:EventEmitter<File> = new EventEmitter<File>();
-  constructor() { }
+	@Output()
+	public afterdrop: EventEmitter<File> = new EventEmitter<File>();
 
-  ngOnInit() {
-  }
+	constructor() {
+	}
 
-  onDragOver($event) {
-    $event.stopPropagation();
-    $event.preventDefault();
-    $event.dataTransfer.dropEffect = 'copy';
-  }
+	@ViewChild("fileinput") fileinput: ElementRef;
 
-  onFileDrop($event) {
-    $event.stopPropagation();
-    $event.preventDefault();
+	ngOnInit() {
+		// Check for the various File API support.
+		if (window.File && window.FileReader && window.FileList && window.Blob) {
+			this.fileAPIsupported = true;
+		}
+	}
 
-    let files = $event.dataTransfer.files; // FileList object.
+	onDragOver($event) {
+		$event.stopPropagation();
+		$event.preventDefault();
+		$event.dataTransfer.dropEffect = 'copy';
+	}
 
-    if (files.length < 1) {
-      alert("Etwas ist schiefgelaufen!");
-    }
-    else {
-      //select the first file
-      this._file = files[ 0 ];
-      this.afterdrop.emit(this._file);
-    }
-  }
+	onFileDrop($event) {
+		$event.stopPropagation();
+		$event.preventDefault();
+
+		if (this.fileAPIsupported) {
+			let files = $event.dataTransfer.files; // FileList object.
+
+			if (files.length == 1) {
+				//select the first file
+				this._file = files[ 0 ];
+				this.afterdrop.emit(this._file);
+			}
+		}
+	}
+
+	onClick($event) {
+		this.fileinput.nativeElement.click();
+	}
+
+	onFileChange($event) {
+		let files = $event.target.files;
+
+		if (files.length == 1) {
+			//select the first file
+			this._file = files[ 0 ];
+			this.afterdrop.emit(this._file);
+		}
+	}
 }

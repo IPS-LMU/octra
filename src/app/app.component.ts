c4ed01c912a114f2d44ea7a0938bce3ba1d49609
Julian Pömp <j.poemp@campus.lmu.de>
2017-03-09 16:10:55 +0100

    fixes #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SessionService } from "./service/session.service";
 import { SettingsService } from "./service/settings.service";
 import { SubscriptionManager } from "./shared/SubscriptionManager";
 import { isNullOrUndefined } from "util";
+import { isUndefined } from "util";
 
 @Component({
 	selector   : 'octra',
@@ -14,7 +15,7 @@ import { isNullOrUndefined } from "util";
 })
 
 export class AppComponent implements OnDestroy {
-	version: string = "1.0.4";
+	version: string = "1.0.5";
 
 	private subscrmanager: SubscriptionManager;
 
@@ -26,11 +27,21 @@ export class AppComponent implements OnDestroy {
 
 		//define languages
 		langService.addLangs([ 'de', 'en' ]);
+		let browser_lang = langService.getBrowserLang();
 
-		if (sessService.language == null || sessService.language == "")
-			langService.setDefaultLang(langService.getBrowserLang());
-		else
+		//check if browser language is available in translations
+		if (isNullOrUndefined(sessService.language) || sessService.language == "") {
+			if (!isUndefined(langService.getLangs().find((value, index, obj) => {
+					return value == browser_lang
+				}))) {
+				langService.use(browser_lang);
+			} else{
+				langService.use("en");
+			}
+		}
+		else {
 			langService.use(sessService.language);
+		}
 
 		//load settings
 		this.subscrmanager.add(this.settingsService.settingsloaded.subscribe(
@@ -40,8 +51,7 @@ export class AppComponent implements OnDestroy {
 					throw "config.json not set correctly";
 				}
 				else {
-					if (this.settingsService.validated)
-					{
+					if (this.settingsService.validated) {
 						this.api.init(this.settingsService.app_settings.audio_server.url + "WebTranscribe");
 					}
 

8fdec4b08976dd5c04a69208ae9dda9716730a8d
Julian Pömp <j.poemp@campus.lmu.de>
2017-04-19 00:19:47 +0200

    fixed issue on relaod transcr-submit page

--- src/app/gui/members-area/members.routes.ts
+++ src/app/gui/members-area/members.routes.ts
@@ -14,7 +14,7 @@ export const MEMBER_ROUTES: Routes = [
 	{ path: '', redirectTo: '/user/load', pathMatch: 'full'},
 	{ path: 'load', component: LoadingComponent, canActivate:[SettingsGuard, MembersAreaGuard]},
 	{ path: 'transcr', component: TranscriptionComponent, canActivate:[SettingsGuard, TranscActivateGuard, MembersAreaGuard]},
-	{ path: 'transcr/submit', component: TranscriptionSubmitComponent, canActivate:[SettingsGuard]},
+	{ path: 'transcr/submit', component: TranscriptionSubmitComponent, canActivate:[SettingsGuard, TranscActivateGuard]},
 	{ path: 'transcr/submitted', component: TranscriptionSubmittedComponent, canActivate:[SettingsGuard, TranscrSubmittedGuard]},
 	{ path: 'transcr/reload-file', component: ReloadFileComponent, canActivate:[ReloadFileGuard, SettingsGuard]},
 ];

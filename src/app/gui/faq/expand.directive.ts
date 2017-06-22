c03e3e2af5cc5948de7e0778a2a2a0f54821d3db
julianpoemp <j.poemp@campus.lmu.de>
2017-06-22 21:40:25 +0200

    changed faq, inserted images

---
+++ src/app/gui/faq/expand.directive.ts
@@ -0,0 +1,52 @@
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appExpand]'
+})
+export class ExpandDirective implements OnInit {
+
+  private toggled = true;
+
+  @HostListener('mousedown') mouseover() {
+    if (this.elRef.nativeElement.tagName !== 'A') {
+      let element: Node = this.renderer.parentNode(this.elRef.nativeElement);
+      element = this.renderer.parentNode(element);
+
+      const td: Node = element.childNodes[3].childNodes[2];
+      this.toogle(td);
+    } else {
+      const element: Node = this.renderer.parentNode(this.elRef.nativeElement);
+
+      const td: Node = element.nextSibling.nextSibling;
+      this.toogle(td);
+    }
+  }
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  ngOnInit() {
+    if (this.elRef.nativeElement.tagName !== 'A') {
+      this.renderer.addClass(this.elRef.nativeElement, 'glyphicon-chevron-up');
+    }
+  }
+
+  private toogle(node: Node) {
+    if (this.toggled) {
+      this.renderer.setStyle(node, 'display', 'inherit');
+      if (this.elRef.nativeElement.tagName !== 'A') {
+        this.renderer.removeClass(this.elRef.nativeElement, 'glyphicon-chevron-up');
+        this.renderer.addClass(this.elRef.nativeElement, 'glyphicon-chevron-down');
+      }
+    } else {
+      this.renderer.setStyle(node, 'display', 'none');
+      if (this.elRef.nativeElement.tagName !== 'A') {
+        this.renderer.removeClass(this.elRef.nativeElement, 'glyphicon-chevron-down');
+        this.renderer.addClass(this.elRef.nativeElement, 'glyphicon-chevron-up');
+      }
+    }
+    this.toggled = !this.toggled;
+  }
+
+}

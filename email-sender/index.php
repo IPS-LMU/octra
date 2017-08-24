9aaab028a490d28831855cd9c447796076616e2e
julianpoemp <j.poemp@campus.lmu.de>
2017-08-24 15:40:13 +0200

    closes #76

---
+++ email-sender/index.php
@@ -0,0 +1,118 @@
+<?php
+// SETTINGS
+$to_email = '';
+$auth_token = '';
+
+// Do not set this in production environment
+// header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Headers: authorization, content-type');
+header('Access-Control-Allow-Methods: POST');
+$data = file_get_contents("php://input");
+
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+$headers = getallheaders();
+
+$result = array(
+    "status" => "failed",
+    "error" => ""
+);
+
+if (!empty($data) && !is_null($headers["Authorization"]) && $headers["Authorization"] === $auth_token) {
+    $data = json_decode($data, true);
+
+    if (!is_null($data["additional_information"]) && !is_null($data["description"]) && !is_null($data["os"]) && !is_null($data["os_build"])
+        && !is_null($data["platform"]) && !is_null($data["version"]) && !is_null($data["report"])) {
+
+        $from_email = $data["additional_information"]["email"];
+        $timestamp = date("Y-m-d-H-i-s");
+        $topic = "OCTRA BUG [$timestamp]";
+        $text = "An user sent a new bug report.<br/><br/>
+        <table>
+        <tr><td> Email Address:</td><td>$from_email</td></tr>
+        <tr><td>Description:</td><td>".$data["description"]."</td></tr>
+        <tr><td>OS:</td><td>" . $data["os"] . " " . $data["os_build"] . "</td></tr>
+        <tr><td>Platform:</td><td>" . $data["platform"] . " " . $data["version"] . "</td></tr>
+</table>
+<br/><br/>
+        Please have a look on the attachment<br><br>
+        --- This message was auto generated ---";
+
+        $file = null;
+        $bugreport = $data["report"];
+        $file_name = "octrabug-$timestamp" . ".txt";
+        $written = file_put_contents($file_name, $bugreport);
+
+        if ($written !== FALSE) {
+            $file = fopen($file_name, 'r');
+        }
+        if (is_null($file)) {
+            $result["status"] = "failed";
+            $result["error"] = "File is null";
+        } else {
+            if (empty($from_email)) {
+                $from_email = "do-not-reply@octra.net";
+            }
+            $attachment = chunk_split(base64_encode($bugreport));
+            $size = filesize($file_name);
+
+            $boundary = "-----=" . md5(uniqid(microtime(), true));
+
+            $header = "From: " . $from_email . "\n";
+            $header .= "Reply-To: " . $from_email . "\n";
+
+            $header .= "MIME-Version: 1.0\r\n";
+            $header .= "Content-Type: multipart/mixed;\r\n";
+            $header .= " boundary=\"" . $boundary . "\"\r\n";
+
+            $content = "This is a multi-part message in MIME format.\r\n\r\n";
+            $content .= "--" . $boundary . "\r\n";
+            $content .= "Content-Type: text/html; charset=\"utf8\"\r\n";
+            $content .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
+            $content .= $text . "\r\n";
+
+            $data = chunk_split(base64_encode($bugreport));
+            $content .= "--" . $boundary . "\r\n";
+            $content .= "Content-Disposition: attachment;\r\n";
+            $content .= "\tfilename=\"" . $file_name . "\";\r\n";
+            $content .= "Content-Length: ." . $size . ";\r\n";
+            $content .= "Content-Type: text/plain; name=\"" . $file_name . "\"\r\n";
+            $content .= "Content-Transfer-Encoding: base64\r\n\r\n";
+            $content .= $data . "\r\n";
+
+            $content .= "--" . $boundary . "--";
+            $send_ok = mail($to_email, $topic, $content, $header);
+
+            if (!$send_ok) {
+                $result["status"] = 'failed';
+                $result["error"] = 'could not send mail';
+            } else {
+                $result["status"] = "success";
+            }
+            unlink($file_name);
+        }
+    } else {
+        $result["status"] = "failed";
+        $result["error"] = "Object posted does not have valid attributes";
+    }
+} else {
+    $result = array(
+        "status" => "failed",
+        "error" => "No Post request or invalid auth token"
+    );
+}
+
+echo json_encode($result);
+
+?>
--- src/app/app.info.ts
+++ src/app/app.info.ts
@@ -8,6 +8,7 @@ import {MantisBugReporter} from './core/obj/BugAPI/MantisBugReporter';
 import {WavFormat} from './core/obj/media/audio/AudioFormats/WavFormat';
 import {OggFormat} from './core/obj/media/audio/AudioFormats/OggFormat';
 import {BundleJSONConverter} from './core/obj/Converters/BundleJSONConverter';
+import {EmailBugReporter} from './core/obj/BugAPI/EmailBugReporter';
 
 export class AppInfo {
   public static readonly audioformats = [
@@ -16,7 +17,8 @@ export class AppInfo {
   ];
 
   public static readonly bugreporters = [
-    new MantisBugReporter()
+    new MantisBugReporter(),
+    new EmailBugReporter()
   ];
 
   public static readonly converters: Converter[] = [

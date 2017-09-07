ab0116d8871c3c32b6ba80361c1280b1fa05f41b
julianpoemp <j.poemp@campus.lmu.de>
2017-09-07 16:04:43 +0200

    about now the news and the faq pages can be fetched from another urls to iframes

--- src/app/core/gui/faq/faq.component.ts
+++ src/app/core/gui/faq/faq.component.ts
@@ -1,16 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {isNullOrUndefined} from 'util';
+import {SettingsService} from '../../shared/service/settings.service';
+import {ProjectConfiguration} from '../../obj/Settings/project-configuration';
+import {NavbarService} from '../navbar/navbar.service';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent implements OnInit, OnChanges {
 
-  constructor() {
+  @Input() url;
+
+  public get projectsettings(): ProjectConfiguration {
+    return this.settService.projectsettings;
+  }
+
+  constructor(private sanitizer: DomSanitizer,
+              private cd: ChangeDetectorRef,
+              public settService: SettingsService,
+              private navService: NavbarService) {
   }
 
   ngOnInit() {
+    this.navService.show_interfaces = false;
+    this.navService.show_export = false;
   }
 
+  ngOnChanges(obj) {
+    if (!isNullOrUndefined(obj.url)) {
+      this.cd.markForCheck();
+      this.cd.checkNoChanges();
+    }
+  }
+
+  get sanitized_url() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.projectsettings.navigation.help_url);
+  }
 }

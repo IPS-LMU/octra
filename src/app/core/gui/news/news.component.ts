ea5b68e877c2f9ab9c1d74feb40869db60202926
julianpoemp <j.poemp@campus.lmu.de>
2017-09-07 16:04:43 +0200

    about now the news and the faq pages can be fetched from another urls to iframes

--- src/app/core/gui/news/news.component.ts
+++ src/app/core/gui/news/news.component.ts
@@ -1,120 +1,43 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Http} from '@angular/http';
-import {SubscriptionManager} from '../../obj/SubscriptionManager';
-import {Functions} from '../../shared/Functions';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 import {isNullOrUndefined} from 'util';
+import {SettingsService} from '../../shared/service/settings.service';
+import {ProjectConfiguration} from '../../obj/Settings/project-configuration';
+import {NavbarService} from '../navbar/navbar.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NewsComponent implements OnInit, OnDestroy {
+export class NewsComponent implements OnInit, OnChanges {
 
-  private subscrmanager: SubscriptionManager = new SubscriptionManager();
-  public app_news: {
-    date: Date,
-    title: string,
-    content: string
-  }[] = [];
-  public prod_news: {
-    date: Date,
-    title: string,
-    content: string
-  }[] = [];
+  @Input() url;
 
-  public app_news_page = 0;
-  public prod_news_page = 0;
-
-  public get Math(): any {
-    return Math;
+  public get projectsettings(): ProjectConfiguration {
+    return this.settService.projectsettings;
   }
 
-  public get app_news_filtered(): any[] {
-    const entries = this.getEntries(this.app_news, this.app_news_page + 1);
-    if (isNullOrUndefined(entries)) {
-      return [];
-    } else {
-      return entries;
-    }
-  };
-
-  public get prod_news_filtered(): any[] {
-    const entries = this.getEntries(this.prod_news, this.prod_news_page + 1);
-    if (isNullOrUndefined(entries)) {
-      return [];
-    } else {
-      return entries;
-    }
-  };
-
-  public MAXNEWSPERPAGE = 5;
-
-  public pages(news: any[]): number[] {
-    const result: number[] = [];
-
-    for (let i = 1; i <= Math.ceil(news.length / this.MAXNEWSPERPAGE); i++) {
-      result.push(i);
-    }
-
-    return result;
-  }
-
-  public getEntries(news: any[], page: number): any[] {
-    const result = [];
-
-    const start = (page === 1) ? 0 : (page - 1) * this.MAXNEWSPERPAGE;
-    let end = ((page - 1) * this.MAXNEWSPERPAGE + this.MAXNEWSPERPAGE);
-    end = Math.min(end, news.length);
-    for (let i = start; i < end; i++) {
-      result.push(news[i])
-    }
-
-    return result;
-  }
-
-  constructor(private http: Http) {
+  constructor(private sanitizer: DomSanitizer,
+              private cd: ChangeDetectorRef,
+              public settService: SettingsService,
+              private navService: NavbarService) {
   }
 
   ngOnInit() {
-    const observable = Functions.uniqueHTTPRequest(this.http, false, null, './assets/contents/app_news.json', null);
-    this.subscrmanager.add(observable.subscribe(
-      (result) => {
-        const news = result.json();
-
-        for (let i = 0; i < news.length; i++) {
-          news[i].date = new Date(news[i].date);
-        }
-
-        this.app_news = news.sort(this.sortArrayByDateDesc);
-      }
-    ));
-
-    const observable2 = Functions.uniqueHTTPRequest(this.http, false, null, './assets/contents/prod_news.json', null);
-    this.subscrmanager.add(observable2.subscribe(
-      (result) => {
-        const news = result.json();
-
-        for (let i = 0; i < news.length; i++) {
-          news[i].date = new Date(news[i].date);
-        }
-
-        this.prod_news = news.sort(this.sortArrayByDateDesc);
-      }
-    ));
+    this.navService.show_interfaces = false;
+    this.navService.show_export = false;
   }
 
-  ngOnDestroy() {
-    this.subscrmanager.destroy();
-  }
-
-  sortArrayByDateDesc = (a, b) => {
-    if (a.date === b.date) {
-      return 0;
-    } else if (a.date < b.date) {
-      return 1;
+  ngOnChanges(obj) {
+    if (!isNullOrUndefined(obj.url)) {
+      this.cd.markForCheck();
+      this.cd.checkNoChanges();
     }
-    return -1;
-  };
+  }
 
+  get sanitized_url() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.projectsettings.navigation.help_url);
+  }
 }

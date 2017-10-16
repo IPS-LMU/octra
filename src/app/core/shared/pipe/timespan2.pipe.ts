f36c42f7f8fb9b8a49db12e7231d72c1f713b74d
julianpoemp <j.poemp@campus.lmu.de>
2017-10-16 23:24:55 +0200

    fixed time format in info box

---
+++ src/app/core/shared/pipe/timespan2.pipe.ts
@@ -0,0 +1,47 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'timespan2',
+  pure: false
+})
+export class Timespan2Pipe implements PipeTransform {
+
+  private timespan = 0;
+
+  transform(value: any, args?: any): any {
+    this.timespan = Number(value);
+    if (this.timespan < 0) {
+      this.timespan = 0;
+    }
+
+    let result = '';
+    const minutes: string = this.FormatNumber(this.Minutes, 2);
+    const seconds: string = this.FormatNumber(this.Seconds, 2);
+    const hours: string = this.FormatNumber(this.Hours, 2);
+
+    result += hours + ':' + minutes + ':' + seconds;
+
+    return result;
+  }
+
+  private get Seconds(): number {
+    return Math.floor(this.timespan / 1000) % 60;
+  }
+
+  private get Minutes(): number {
+    return Math.floor(this.timespan / 1000 / 60);
+  }
+
+  private get Hours(): number {
+    return Math.floor(this.timespan / 1000 / 60 / 60);
+  }
+
+  private FormatNumber = (num, length): string => {
+    let result = '' + num.toFixed(0);
+    while (result.length < length) {
+      result = '0' + result;
+    }
+    return result;
+  }
+
+}

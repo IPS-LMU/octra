eebe876a1b106f277f82e52069d8a7b2e7dddbb3
julianpoemp <j.poemp@campus.lmu.de>
2019-04-12 15:59:17 +0200

    implemented MultiThreadedService for organization of web workers

---
+++ src/app/core/shared/multi-threading/ts-worker-job.ts
@@ -0,0 +1,69 @@
+export enum TsWorkerStatus {
+  INITIALIZED = 'initialized',
+  RUNNING = 'running',
+  FINISHED = 'finished',
+  FAILED = 'failed',
+  STOPPED = 'stopped'
+}
+
+export class TsWorkerJob {
+  get result(): any {
+    return this._result;
+  }
+
+  set result(value: any) {
+    this._result = value;
+  }
+
+  set statistics(value: { ended: number; started: number }) {
+    this._statistics = value;
+  }
+
+  get statistics(): { ended: number; started: number } {
+    return this._statistics;
+  }
+
+  get status(): TsWorkerStatus {
+    return this._status;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  private static jobIDCounter = 0;
+  private _statistics = {
+    started: -1,
+    ended: -1
+  };
+
+  private _id: number;
+  private _result: any;
+  private _status: TsWorkerStatus = TsWorkerStatus.INITIALIZED;
+
+  args: any[] = [];
+
+  constructor(doFunction: (args: any[]) => Promise<any>, args: any[]) {
+    this._id = ++TsWorkerJob.jobIDCounter;
+    this.doFunction = doFunction;
+    this.args = args;
+  }
+
+  /**
+   * this function will be run in the web worker
+   * @param args
+   */
+  doFunction = (args: any[]) => {
+    return new Promise<any>((resolve, reject) => {
+      reject('not implemented');
+    });
+  }
+
+  /**
+   * changes this job's status
+   * @param status the status to change
+   */
+  public changeStatus(status: TsWorkerStatus) {
+    this._status = status;
+  }
+}

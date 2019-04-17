9d7b5e6185d628836386bb900f3994d2d8b8a5ea
julianpoemp <j.poemp@campus.lmu.de>
2019-04-17 11:05:03 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-buildDir="dist/"
-appName="octra-dev"
+buildDir="dist/octra/"
+baseHref="https://www.phonetik.uni-muenchen.de/apps/octra/octra-dev/"
+
+# change this list to your needs (for exclusion of files or folders in the dist folder)
+excludedList='"config" "LICENCE.txt"'
+
+# 1 = disable indexing, 2 = enable
 disableRobots=1
 
 echo "Building OCTRA..."
-ng build --prod --base-href "https://www.phonetik.uni-muenchen.de/apps/octra/${appName}/"
+ng build --prod --base-href "${baseHref}"
 echo "Change index.html..."
 indexHTML=$(<${buildDir}index.html)
 indexHTML=$(echo "${indexHTML}" | sed -e 's/\(scripts\.[0-9a-z]*\.js\)/.\/assets\/\1/g')
-indexHTML=$(echo "${indexHTML}" | sed -e 's/\(polyfills\.[0-9a-z]*\.js\)/.\/assets\/\1/g')
+indexHTML=$(echo "${indexHTML}" | sed -e 's/\(es2015-polyfills\.[0-9a-z]*\.js\)/.\/assets\/\1/g')
+indexHTML=$(echo "${indexHTML}" | sed -e 's/\(src="\)\(polyfills\.[0-9a-z]*\.js\)/\1.\/assets\/\2/g')
 indexHTML=$(echo "${indexHTML}" | sed -e 's/\(main\.[0-9a-z]*\.js\)/.\/assets\/\1/g')
 indexHTML=$(echo "${indexHTML}" | sed -e 's/\(runtime\.[0-9a-z]*\.js\)/.\/assets\/\1/g')
 indexHTML=$(echo "${indexHTML}" | sed -e 's/\(styles\.[0-9a-z]*\.css\)/.\/assets\/\1/g')
@@ -21,14 +27,32 @@ fi
 
 echo "${indexHTML}" > ${buildDir}index.html
 
-for old in ./${buildDir}*; do
-    new=$(echo $old | sed -e 's/\.\/dist\///g')
-    if [[ ${new} != 'index.html' ]] && [[ ${new} != 'assets' ]] && [[ ${new} != 'config' ]] && [[ ${new} != 'LICENSE.txt' ]]
-    then
-      mv "./${buildDir}${new}" "./${buildDir}assets/${new}"
-    fi
-    #mv -v "$old" "$new"
-  done
+regEx=$(echo "${buildDir}" | sed 's:\/:\\/:g')
+regEx="s|\.\/${regEx}||g"
+echo ${regEx}
 
-mv "./${buildDir}assets/.htaccess" "./${buildDir}.htaccess"
+for old in ./${buildDir}*; do
+    entry=$(echo $old | sed -e "${regEx}")
+    found=0
+
+    if [[ ${excludedList} != '' ]]
+    then
+      for excluded in ${excludedList}; do
+        echo "compare ${entry} with ${excluded}"
+        if [[ "\"${entry}\"" == ${excluded} ]]
+        then
+          found=1
+          break
+        fi
+      done
+    fi
+
+    if [[ ${entry} != 'index.html' ]] && [[ ${entry} != 'assets' ]] && [[ ${found} == 0 ]]
+    then
+      mv "./${buildDir}${entry}" "./${buildDir}assets/${entry}"
+    fi
+  done
+# you can add more jobs here
+# mv "./${buildDir}assets/.htaccess" "./${buildDir}.htaccess"
+
 echo "Building COMPLETE"

c2da8ebc390041a34284efdac60fe299ffdf2610
Julian Pömp <j.poemp@campus.lmu.de>
2020-02-07 00:52:25 +0100

    updated other files for angular 9 upgrade

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('octra-statistics app is running!');
+    expect(page.getTitleText()).toEqual('tt app is running!');
   });
 
   afterEach(async () => {

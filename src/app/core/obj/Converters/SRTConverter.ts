738294bfaaf54453a8e14100d55191f6f059569a
Julian Pömp <jp@poemp.net>
2023-08-07 17:06:24 +0200

    fix(converters): missing special character validation of backslash r

--- src/app/core/obj/Converters/SRTConverter.ts
+++ src/app/core/obj/Converters/SRTConverter.ts
@@ -1,28 +1,32 @@
 import {Converter, ExportResult, IFile, ImportResult} from './Converter';
-import {OAnnotJSON, OAudiofile, OLabel, OLevel, OSegment} from '../Annotation';
+import {OAnnotJSON, OLabel, OLevel, OSegment} from '../Annotation/AnnotJSON';
+import {OAudiofile} from '../Annotation';
 
 export class SRTConverter extends Converter {
-
   public constructor() {
     super();
     this._application = 'Video';
     this._name = 'SRT Subtitles';
     this._extension = '.srt';
     this._website.title = 'SRT Subtitles';
-    this._website.url = 'https://matroska.org/technical/specs/subtitles/srt.html';
+    this._website.url =
+      'https://matroska.org/technical/specs/subtitles/srt.html';
     this._conversion.export = true;
     this._conversion.import = true;
     this._encoding = 'UTF-8';
     this._multitiers = false;
   }
 
-  public static getSamplesFromTimeString(timeString: string, sampleRate: number) {
+  public static getSamplesFromTimeString(
+    timeString: string,
+    sampleRate: number
+  ) {
     if (sampleRate > 0) {
       const regex = new RegExp(/([0-9]+):([0-9]+):([0-9]+),([0-9]+)/g);
 
       const matches = regex.exec(timeString);
 
-      if (matches.length > -1) {
+      if (matches && matches.length > -1) {
         const hours = Number(matches[1]);
         const minutes = Number(matches[2]);
         const seconds = Number(matches[3]);
@@ -35,60 +39,81 @@ export class SRTConverter extends Converter {
         totalMiliSeconds += miliseconds;
         totalMiliSeconds = Math.round(totalMiliSeconds);
 
-        return Math.round(totalMiliSeconds / 1000 * sampleRate);
+        return Math.round((totalMiliSeconds / 1000) * sampleRate);
       } else {
         console.error(`time string does not match`);
       }
     } else {
       console.error(`invalid sample rate`);
     }
+    return -1;
   }
 
-  public export(annotation: OAnnotJSON, audiofile: OAudiofile, levelnum: number): ExportResult {
-    let result = '';
-    let filename = '';
+  public export(
+    annotation: OAnnotJSON,
+    audiofile: OAudiofile,
+    levelnum: number
+  ): ExportResult | undefined {
+    if (annotation) {
+      let result = '';
+      let filename = '';
 
-    if (!(levelnum === null || levelnum === undefined) && levelnum < annotation.levels.length) {
-      const level: OLevel = annotation.levels[levelnum];
+      if (
+        levelnum === undefined ||
+        levelnum < 0 ||
+        levelnum > annotation.levels.length
+      ) {
+        console.error('SRTConverter needs a level number');
+        return undefined;
+      }
 
-      let counter = 1;
-      if (level.type === 'SEGMENT') {
-        for (let j = 0; j < level.items.length; j++) {
-          const transcript = level.items[j].labels[0].value;
-          const start = this.getTimeStringFromSamples(level.items[j].sampleStart, annotation.sampleRate);
-          const end = this.getTimeStringFromSamples(level.items[j].sampleStart + level.items[j].sampleDur, annotation.sampleRate);
+      if (levelnum < annotation.levels.length) {
+        const level: OLevel = annotation.levels[levelnum];
 
-          if (transcript !== '') {
-            result += `${counter}\n`;
-            result += `${start} --> ${end}\n`;
-            result += `${transcript}\n\n`;
-            counter++;
+        let counter = 1;
+        if (level.type === 'SEGMENT') {
+          for (const item of level.items) {
+            const transcript = item.labels[0].value;
+            const start = this.getTimeStringFromSamples(
+              item.sampleStart!,
+              annotation.sampleRate
+            );
+            const end = this.getTimeStringFromSamples(
+              item.sampleStart! + item.sampleDur!,
+              annotation.sampleRate
+            );
+
+            if (transcript !== '') {
+              result += `${counter}\n`;
+              result += `${start} --> ${end}\n`;
+              result += `${transcript}\n\n`;
+              counter++;
+            }
           }
         }
+
+        filename = `${annotation.name}`;
+        if (annotation.levels.length > 1) {
+          filename += `-${level.name}`;
+        }
+        filename += `${this._extension}`;
       }
 
-      filename = `${annotation.name}`;
-      if (annotation.levels.length > 1) {
-        filename += `-${level.name}`;
-      }
-      filename += `${this._extension}`;
-    } else {
-      console.error('SRTConverter needs a level number');
-      return null;
+      return {
+        file: {
+          name: filename,
+          content: result,
+          encoding: 'UTF-8',
+          type: 'text/plain',
+        },
+      };
     }
-
-    return {
-      file: {
-        name: filename,
-        content: result,
-        encoding: 'UTF-8',
-        type: 'text/plain'
-      }
-    };
+    console.error(`annotation file is undefined`);
+    return undefined;
   }
 
   public import(file: IFile, audiofile: OAudiofile): ImportResult {
-    if (audiofile !== null && audiofile !== undefined) {
+    if (audiofile) {
       const result = new OAnnotJSON(audiofile.name, audiofile.samplerate);
 
       const content = file.content;
@@ -98,26 +123,37 @@ export class SRTConverter extends Converter {
       let lastEnd = 0;
 
       if (content !== '') {
-        const regex = new RegExp('([0-9]+)\n([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> ([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})\n' +
-          '((?:(?:(?![0-9]).+)?\n?)+)', 'g');
+        const regex =
+          /([0-9]+)[\n\r]*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> ([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})[\n\r]*((?:(?:(?![0-9]).+)?[\n\r]*)+)/g;
 
         let matches = regex.exec(content);
         while (matches !== null) {
-          const timeStart = SRTConverter.getSamplesFromTimeString(matches[2], audiofile.samplerate);
-          const timeEnd = SRTConverter.getSamplesFromTimeString(matches[3], audiofile.samplerate);
+          const timeStart = SRTConverter.getSamplesFromTimeString(
+            matches[2],
+            audiofile.samplerate
+          );
+          const timeEnd = SRTConverter.getSamplesFromTimeString(
+            matches[3],
+            audiofile.samplerate
+          );
           const segmentContent = matches[4].replace(/(\n|\s)+$/g, '');
 
-          if (timeStart > lastEnd) {
-            // add additional segment
-            olevel.items.push(new OSegment(
-              counterID++, lastEnd, timeStart - lastEnd, [new OLabel('OCTRA_1', '')]
-            ));
+          if (timeStart > -1 && timeEnd > -1) {
+            if (timeStart > lastEnd) {
+              // add additional segment
+              olevel.items.push(
+                new OSegment(counterID++, lastEnd, timeStart - lastEnd, [
+                  new OLabel('OCTRA_1', ''),
+                ])
+              );
+            }
+
+            olevel.items.push(
+              new OSegment(counterID++, timeStart, timeEnd - timeStart, [
+                new OLabel('OCTRA_1', segmentContent),
+              ])
+            );
           }
-
-          olevel.items.push(new OSegment(
-            counterID++, timeStart, timeEnd - timeStart, [new OLabel('OCTRA_1', segmentContent)]
-          ));
-
           matches = regex.exec(content);
           lastEnd = timeEnd;
         }
@@ -126,26 +162,33 @@ export class SRTConverter extends Converter {
       if (olevel.items.length > 0) {
         // set last segment duration to fit last sample
         const lastItem = olevel.items[olevel.items.length - 1];
-        olevel.items[olevel.items.length - 1].sampleDur = Number(audiofile.duration) - Number(lastItem.sampleStart);
-      }
+        olevel.items[olevel.items.length - 1].sampleDur =
+          Number(audiofile.duration) - Number(lastItem.sampleStart);
 
-      result.levels.push(olevel);
-      return {
-        annotjson: result,
-        audiofile: null,
-        error: ''
-      };
+        result.levels.push(olevel);
+        return {
+          annotjson: result,
+          audiofile: undefined,
+          error: '',
+        };
+      } else {
+        return {
+          annotjson: undefined,
+          audiofile: undefined,
+          error: 'Input file is not comatible with SRT format.',
+        };
+      }
     }
 
     return {
-      annotjson: null,
-      audiofile: null,
-      error: `This SRT file is not compatible with this audio file.`
+      annotjson: undefined,
+      audiofile: undefined,
+      error: `This SRT file is not compatible with this audio file.`,
     };
   }
 
   public getTimeStringFromSamples(samples: number, sampleRate: number) {
-    const miliseconds = Math.round(samples / sampleRate * 1000);
+    const miliseconds = Math.round((samples / sampleRate) * 1000);
     const seconds = Math.floor(miliseconds / 1000);
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
@@ -158,11 +201,11 @@ export class SRTConverter extends Converter {
     return `${hoursStr}:${minutesStr}:${secondsStr},${miliStr}`;
   }
 
-  public formatNumber = (num, length): string => {
+  public formatNumber = (num: number, length: number): string => {
     let result = '' + num.toFixed(0);
     while (result.length < length) {
       result = '0' + result;
     }
     return result;
-  }
+  };
 }

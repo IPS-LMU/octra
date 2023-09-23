8bdc9644fc8cb9f1f989f0af705968fc5d17f8db
Julian Pömp <jp@poemp.net>
2023-09-23 12:30:19 +0200

    refactor(project): fix ngx-utilities exports

--- libs/ngx-utilities/src/index.ts
+++ libs/ngx-utilities/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/functions';
 export * from './lib/pipes';
+export * from './lib/octra-utilities.module';
--- libs/ngx-utilities/src/lib/pipes/index.ts
+++ libs/ngx-utilities/src/lib/pipes/index.ts
@@ -1,4 +1,3 @@
 export * from './leadingnull.pipe';
 export * from './procent.pipe';
 export * from './timespan.pipe';
-export * from './octra-utilities.module';

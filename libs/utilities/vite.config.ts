5cefe60a96bf49b843017212e13879aa8a28ee15
Julian Pömp <jp@poemp.net>
2023-11-24 11:21:57 +0100

    fix(utilities): missing typings

--- libs/utilities/vite.config.ts
+++ libs/utilities/vite.config.ts
@@ -1,10 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   cacheDir: '../node_modules/.vite/utilities',
 
-  plugins: [],
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+      skipDiagnostics: true,
+    }),
+    nxViteTsPaths(),
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {

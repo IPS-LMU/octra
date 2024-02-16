e527f399f1032219c086105905db2281906a2535
Julian Pömp <jp@poemp.net>
2024-02-16 10:24:13 +0100

    feat(assets): add JSON definitions inputs and outputs

--- libs/assets/src/lib/schemata/index.ts
+++ libs/assets/src/lib/schemata/index.ts
@@ -1,3 +1,10 @@
+import io from './inputs_outputs.set.json';
+
 export * from './guidelines';
 export * from './logging.schema';
 export * from './projectconfig.schema';
+
+export const IO_SETS = {
+  inputs: io.inputs,
+  outputs: io.outputs
+};
--- runner.js
+++ runner.js
@@ -4,11 +4,11 @@ const fs = require('fs-extra');
 const process = require('node:child_process');
 const { exec } = require('node:child_process');
 const { pathExists } = require('@nx/eslint-plugin/src/utils/graph-utils');
-const crypto = require("crypto");
+const crypto = require('crypto');
 
 const JSONValidator = {
   build: async function () {
-    await bundleLibrary("json-sets");
+    await bundleLibrary('json-sets');
   },
 };
 
@@ -34,9 +34,18 @@ async function bundleLibrary(libraryName) {
 
 async function buildLibrary(libraryName) {
   await run(`nx build ${libraryName}`);
-  await fs.copyFile(`libs/${libraryName}/LICENSE.txt`, `dist/libs/${libraryName}/LICENSE.txt`);
-  await fs.copyFile(`libs/${libraryName}/README.md`, `dist/libs/${libraryName}/README.md`);
-  await fs.copyFile(`libs/${libraryName}/CHANGELOG.md`, `dist/libs/${libraryName}/CHANGELOG.md`);
+  await fs.copyFile(
+    `libs/${libraryName}/LICENSE.txt`,
+    `dist/libs/${libraryName}/LICENSE.txt`
+  );
+  await fs.copyFile(
+    `libs/${libraryName}/README.md`,
+    `dist/libs/${libraryName}/README.md`
+  );
+  await fs.copyFile(
+    `libs/${libraryName}/CHANGELOG.md`,
+    `dist/libs/${libraryName}/CHANGELOG.md`
+  );
 }
 
 const Project = {
@@ -107,10 +116,29 @@ const OCTRA = {
     await run(`npm run build:web-components`);
     await JSONValidator.build();
     await bundleLibrary('utilities');
-    await bundleLibrary('assets');
+    await this.bundleAssets();
     await bundleLibrary('annotation');
     await bundleLibrary('media');
     await bundleLibrary('web-media');
+  },
+  bundleAssets: async function () {
+    await buildLibrary('assets');
+    await fs.copyFile(
+      './libs/assets/src/lib/schemata/inputs_outputs.set.json',
+      './dist/libs/assets/lib/schemata/inputs_outputs.set.json'
+    );
+    await fs.copyFile(
+      './libs/assets/src/lib/schemata/guidelines.schema.json',
+      './dist/libs/assets/lib/schemata/guidelines.schema.json'
+    );
+    await fs.copyFile(
+      './libs/assets/src/lib/schemata/logging.schema.json',
+      './dist/libs/assets/lib/schemata/logging.schema.json'
+    );
+    await fs.copyFile(
+      './libs/assets/src/lib/schemata/projectconfig.schema.json',
+      './dist/libs/assets/lib/schemata/projectconfig.schema.json'
+    );
   },
 };
 
@@ -128,11 +156,18 @@ yargs
   .command('build:dev', 'Builds development version of OCTRA.', OCTRA.buildDev)
   .command('build:beta', 'Builds beta version of OCTRA.', OCTRA.buildBeta)
   .command('build:libs', 'Builds all libraries.', OCTRA.buildLibs)
-  .command('build:lib [library]', 'Builds a library.', (yargs) => yargs.positional("library", {
-    describe: "library name"
-  }), (argv) => {
-    buildLibrary(argv.library)
-  })
+  .command('bundle:assets', 'Bundle assets.', OCTRA.bundleAssets)
+  .command(
+    'build:lib [library]',
+    'Builds a library.',
+    (yargs) =>
+      yargs.positional('library', {
+        describe: 'library name',
+      }),
+    (argv) => {
+      buildLibrary(argv.library);
+    }
+  )
   .command('build:extern', 'Builds extern libraries.', OCTRA.buildExtern)
   .command('build:json-sets', 'Builds json-sets library.', JSONValidator.build)
   .command(
@@ -211,7 +246,9 @@ async function setBuildVariable() {
     return `${g1} ${JSON.stringify(build)}${g3}`;
   });
 
-  await fs.writeFile('./dist/apps/octra/index.html', content, { encoding: 'utf-8' });
+  await fs.writeFile('./dist/apps/octra/index.html', content, {
+    encoding: 'utf-8',
+  });
 
   console.log(`BUILD-HASH: ${hash}`);
 }

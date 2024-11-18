be0151749a89a5c20ee7994bd362f014c699894f
Julian Pömp <jp@poemp.net>
2024-11-18 12:58:51 +0100

    feat(ngx-components): move bugreport modal to ngx-components

---
+++ libs/ngx-components/src/lib/components/bugreport-modal/types.ts
@@ -0,0 +1,15 @@
+export interface BugReportTranslations {
+  giveFeedback?: string;
+  error?: string;
+  introduction?: string;
+  bugReportSent?: string;
+  addProtocol?: string;
+  name?: string;
+  eMail?: string;
+  description?: string;
+  screenshots?: string;
+  protocol?: string;
+  sendFeedback?: string;
+  abort?: string;
+  sending?: string;
+}

1b8edc854d4afbe82963db66104ea2a444fc17e1
Julian Pömp <jp@poemp.net>
2025-10-17 15:27:48 +0200

    feat(annotation): PartiturConverter now supports WOR in combination with SPK tiers

--- libs/annotation/src/lib/converters/PartiturConverter.ts
+++ libs/annotation/src/lib/converters/PartiturConverter.ts
@@ -1,5 +1,5 @@
 import { OAudiofile } from '@octra/media';
-import { last } from '@octra/utilities';
+import { ensureNumber, last } from '@octra/utilities';
 import { FileInfo } from '@octra/web-media';
 import { OAnnotJSON, OLabel, OSegment, OSegmentLevel } from '../annotjson';
 import {
@@ -13,6 +13,99 @@ import {
   BASWebservicesApplication,
   OctraApplication,
 } from './SupportedApplications';
+
+class ParsedBASPartitur {
+  LHD!: string;
+  SNB?: number;
+  SAM?: number;
+  SBF?: string;
+  SSB?: number;
+  NCH?: number;
+  SAO?: string;
+  LBD?: string;
+
+  KAN: string[] = [];
+  ORT: string[] = [];
+  TRO: string[] = [];
+  WOR: {
+    start: number;
+    duration: number;
+    ortIndex: number;
+    value: string;
+  }[] = [];
+  SPK: {
+    ortIndex: number;
+    value: string;
+  }[] = [];
+  TRN: {
+    start: number;
+    duration: number;
+    value: string;
+  }[] = [];
+
+  parse(lines: string[]) {
+    for (const line of lines) {
+      const search = line.match(
+        new RegExp(
+          '^((LHD)|(SAM)|(KAN)|(ORT)|(DAS)|(TR2)|(SUP)|(PRS)|(NOI)|(LBP)|(LBG)|(PRO)|(POS)|(LMA)|(SYN)|(FUN)|(LEX)|' +
+            '(IPA)|(TRN)|(TRS)|(GES)|(USH)|(USM)|(OCC)|(USP)|(GES)|(TLN)|(PRM)|(TRW)|(MAS)|(SPK)|(TRO)|(WOR))',
+          'g',
+        ),
+      );
+
+      if (search) {
+        const columns = line.split(/[\s\t]+/g);
+
+        if (columns[0] === 'LHD:') {
+          this.LHD = `${columns[1]} ${columns[2]}`;
+        } else if (columns[0] === 'SNB:') {
+          this.SNB = ensureNumber(columns[1]);
+        } else if (columns[0] === 'SAM:') {
+          this.SAM = ensureNumber(columns[1]);
+        } else if (columns[0] === 'SBF:') {
+          this.SBF = columns[1];
+        } else if (columns[0] === 'SSB:') {
+          this.SSB = ensureNumber(columns[1]);
+        } else if (columns[0] === 'NCH:') {
+          this.NCH = ensureNumber(columns[1]);
+        } else if (columns[0] === 'SAO:') {
+          this.SAO = columns.slice(1).join(' ');
+        } else if (columns[0] === 'LBD:') {
+          this.LBD = columns[1] ?? '';
+        } else if (columns[0] === 'KAN:') {
+          this.KAN.push(columns.slice(2).join(' '));
+        } else if (columns[0] === 'ORT:') {
+          this.ORT.push(columns[2]);
+        } else if (columns[0] === 'SPK:') {
+          this.SPK.push({
+            ortIndex: ensureNumber(columns[1]) ?? -1,
+            value: columns.slice(2).join(' '),
+          });
+        } else if (columns[0] === 'TRO:') {
+          this.TRO.push(columns[2]);
+        } else if (columns[0] === 'WOR:') {
+          this.WOR.push({
+            start: ensureNumber(columns[1]) ?? -1,
+            duration: ensureNumber(columns[2]) ?? -1,
+            ortIndex: ensureNumber(columns[3]) ?? -1,
+            value: columns[4],
+          });
+        } else if (columns[0] === 'TRN:') {
+          const parsed = /(TRN): ([0-9]+) ([0-9]+) (?:[0-9]+,?)+ (.*)/g.exec(
+            line,
+          );
+          if (parsed) {
+            this.TRN.push({
+              start: ensureNumber(parsed[2])!,
+              duration: ensureNumber(parsed[3])!,
+              value: parsed[4],
+            });
+          }
+        }
+      }
+    }
+  }
+}
 
 // http://www.bas.uni-muenchen.de/Bas/BasFormatsdeu.html
 export class PartiturConverter extends Converter {
@@ -157,127 +250,145 @@ LBD:\n`;
     }
 
     const lines = file.content.split(/\r?\n/g);
-    let pointer = 0;
-
     const result = new OAnnotJSON(
       audiofile.name,
       FileInfo.extractFileName(file.name).name,
       audiofile.sampleRate,
     );
 
+    const parsedPartitur = new ParsedBASPartitur();
+    parsedPartitur.parse(lines);
+
+    if (!parsedPartitur.SAM || parsedPartitur.SAM !== audiofile.sampleRate) {
+      return {
+        error: `Sample Rate of audio file is not equal to the value from Partitur`,
+      };
+    }
+
     // skip not needed information and read needed information
     let counter = 1;
-    const speakers: string[] = [];
 
-    while (pointer < lines.length) {
-      const search = lines[pointer].match(
-        new RegExp(
-          '^((LHD)|(SAM)|(KAN)|(ORT)|(DAS)|(TR2)|(SUP)|(PRS)|(NOI)|(LBP)|(LBG)|(PRO)|(POS)|(LMA)|(SYN)|(FUN)|(LEX)|' +
-            '(IPA)|(TRN)|(TRS)|(GES)|(USH)|(USM)|(OCC)|(USP)|(GES)|(TLN)|(PRM)|(TRW)|(MAS)|(SPK))',
-          'g',
-        ),
-      );
+    if (parsedPartitur.SPK.length === 0 || parsedPartitur.WOR.length === 0) {
+      // fallback to TRN
+      if (parsedPartitur.TRN.length > 0) {
+        const level = new OSegmentLevel<OSegment>('TRN');
 
-      if (search) {
-        const columns = lines[pointer].split(/[\s\t]+/g);
+        for (const item of parsedPartitur.TRN) {
+          level.items.push(
+            new OSegment(counter++, item.start, item.duration, [
+              new OLabel('TRN', item.value),
+            ]),
+          );
+        }
 
-        if (search[0] === 'SAM') {
-          if (audiofile.sampleRate !== Number(columns[1])) {
-            console.error(
-              `Sample Rate of audio file is not equal to the value from Partitur` +
-                ` file! ${audiofile.sampleRate} !== ${columns[1]}`,
+        result.levels.push(level);
+      }
+    } else {
+      // found speakers. We need to read all WOR items and assign them to levels
+      if (parsedPartitur.WOR.length === 0) {
+        return {
+          error: "Can't read Partitur file with SPK but missing WOR items.",
+        };
+      } else {
+        for (const worElement of parsedPartitur.WOR) {
+          if (worElement.ortIndex > -1) {
+            const speaker =
+              parsedPartitur.SPK.find((a) => a.ortIndex === worElement.ortIndex)
+                ?.value ?? 'NA';
+            let level: OSegmentLevel<OSegment> | undefined = result.levels.find(
+              (a) => a.name === speaker,
+            ) as any;
+
+            if (!level) {
+              level = new OSegmentLevel(speaker);
+              result.levels.push(level);
+            }
+
+            level.items.push(
+              new OSegment(counter++, worElement.start, worElement.duration, [
+                new OLabel(speaker, worElement.value),
+                new OLabel('Speaker', speaker),
+              ]),
             );
           }
-        } else if (search[0] === 'SPK') {
-          speakers.push(columns[2]);
-        } else if (search[0] === 'TRN') {
-          const transcript = lines[pointer];
-          const transcriptArray = transcript.match(
-            /TRN:[\s\t]+([0-9]+)[\s\t]([0-9]+)[\s\t]+((?:[0-9],?)+)[\s\t]+(.*)/,
-          );
+        }
+      }
+    }
 
-          if (transcriptArray) {
-            const startSample = Number(transcriptArray[1]);
-            const durSample = Number(transcriptArray[2]);
-            const affectedUnits = transcriptArray[3]
-              .split(',')
-              .filter((a) => a !== undefined && a !== null && a !== '');
-            const transcript = transcriptArray[4];
+    // normalize levels
+    if (result.levels.length > 0) {
+      for (const level of result.levels as OSegmentLevel<OSegment>[]) {
+        for (let i = 0; i < level.items.length; i++) {
+          const item = level.items[i];
+          const previousItem = i > 0 ? level.items[i - 1] : undefined;
+          const startSample =
+            (previousItem?.sampleStart ?? 0) + (previousItem?.sampleDur ?? 0);
+          const gapSamples = item.sampleStart - startSample;
+          const gapSeconds = gapSamples / audiofile.sampleRate;
 
-            if (affectedUnits.length > 0) {
-              const speakerIndex = Number(affectedUnits[0]);
-              const speaker = speakers
-                ? (speakers[speakerIndex] ?? 'TRN')
-                : 'TRN';
-              let level: OSegmentLevel<OSegment> | undefined =
-                result.levels.find((a) => a.name === speaker) as
-                  | OSegmentLevel<OSegment>
-                  | undefined;
+          if (gapSamples !== 0) {
+            // gap
+            if (gapSeconds < 1) {
+              // change sampleStart of first segment to startSample
+              item.sampleDur += gapSamples;
+              item.sampleStart = startSample;
+            } else {
+              // fill with new segment
 
-              if (!level) {
-                level = new OSegmentLevel(speaker);
-                result.levels.push(level);
-              }
-
-              let previousItem: OSegment | undefined = last(
-                level.items,
-              ) as OSegment;
-
-              if (!previousItem && startSample > 0) {
-                previousItem = new OSegment(counter++, 0, startSample, [
-                  new OLabel(speaker, ''),
-                ]);
-                level.items.push(previousItem);
-              } else if (
-                previousItem &&
-                previousItem.sampleStart + previousItem.sampleDur < startSample
-              ) {
-                // fill with empty segment
-                previousItem = new OSegment(
-                  counter++,
-                  previousItem.sampleStart + previousItem.sampleDur,
-                  startSample -
-                    (previousItem.sampleStart + previousItem.sampleDur),
-                  [new OLabel(speaker, '')],
-                );
-                level.items.push(previousItem);
-              }
-
-              level.items.push(
-                new OSegment(counter++, startSample, durSample, [
-                  new OLabel(speaker, transcript),
-                  new OLabel('Speaker', speaker),
-                ]),
+              const test = item.sampleStart - (startSample + gapSamples);
+              const newItem = new OSegment(counter++, startSample, gapSamples, [
+                new OLabel(level.name, ''),
+              ]);
+              const speakerLabel = item.labels.find(
+                (a) => a.name !== level.name,
               );
+              if (speakerLabel) {
+                newItem.labels.push(speakerLabel.clone());
+              }
+              level.items = [
+                ...level.items.slice(0, i),
+                newItem,
+                ...level.items.slice(i),
+              ];
+              i++;
             }
           }
         }
       }
-      pointer++;
-    }
-    if (result.levels.length > 0) {
-      for (const level1 of result.levels as OSegmentLevel<OSegment>[]) {
-        const lastItem = last(level1.items) as OSegment;
 
-        if (
-          lastItem &&
-          lastItem.sampleStart + lastItem.sampleDur < audiofile.duration
-        ) {
-          const gap =
-            (audiofile.duration - lastItem.sampleStart) / audiofile.sampleRate;
-          if (gap < 1) {
-            //less than 1 second > concat
-            lastItem.sampleDur = audiofile.duration - lastItem.sampleStart;
-          } else {
-            // fill gap with empty unit
-            const start =
-              audiofile.duration - lastItem.sampleStart - lastItem.sampleDur;
-            level1.items.push(
-              new OSegment(counter++, start, audiofile.duration - start, [
-                new OLabel(level1.name, ''),
-                new OLabel('Speaker', level1.name),
-              ]),
-            );
+      for (const level of result.levels as OSegmentLevel<OSegment>[]) {
+        const lastItem = last(level.items);
+
+        if (!lastItem) {
+          level.items.push(
+            new OSegment(counter++, 0, audiofile.duration, [
+              new OLabel('OCTRA_1', ''),
+            ]),
+          );
+        } else {
+          const gapSamples =
+            audiofile.duration - (lastItem.sampleStart + lastItem.sampleDur);
+          const gapSeconds = gapSamples / audiofile.sampleRate;
+
+          if (gapSeconds !== 0) {
+            if (gapSeconds < 1) {
+              lastItem.sampleDur += gapSamples;
+            } else {
+              // fill with empty item
+              const newItem = new OSegment(
+                counter++,
+                lastItem.sampleStart + lastItem.sampleDur,
+                gapSamples,
+                [new OLabel(level.name, '')],
+              );
+              const speakerLabel = lastItem.labels.find(
+                (a) => a.name !== level.name,
+              );
+              if (speakerLabel) {
+                newItem.labels.push(speakerLabel.clone());
+              }
+              level.items.push(newItem);
+            }
           }
         }
       }
